e94e760918c56267ff5a9676216284278b3875e7
eccentric.node <austin.b.johnson98@gmail.com>
2023-04-30 16:07:13 -0700

    added some needed plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image()],
+  output: "server",
+  adapter: netlify()
+});
