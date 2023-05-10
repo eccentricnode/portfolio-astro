4902d9ce868caa7dfa9a4b3809056e981dc91425
eccentricnode <austin.b.johnson98@gmail.com>
2023-05-09 19:12:01 -0700

    some more changes, odds and ends, as well as adding form specific useful functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
-
 import netlify from "@astrojs/netlify/functions";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
+  integrations: [image(), sitemap()],
   output: "server",
   adapter: netlify()
 });
